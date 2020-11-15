cfdd1d740c7f0f1de88d6fbfe14135fcdcc3d871
HaniSinnoBG <hanisinno1999@gmail.com>
2020-11-15 16:44:32 +0200

    innitial database models added

---
+++ BreakDanceCompetitions/Data/BreakDanceBattles.Data.Models/Category.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreakDanceBattles.Data.Models
+{
+    public class Category
+    {
+    }
+}
---
+++ BreakDanceCompetitions/Data/BreakDanceBattles.Data.Models/Competition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreakDanceBattles.Data.Models
+{
+    public class Competition
+    {   
+        
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string AddedByUserId { get; set; }
+
+        //country
+
+      
+
+
+    }
+}
